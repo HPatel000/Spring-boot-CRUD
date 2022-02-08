0d2697ed383b6803e00f7c4ccbe5147655d32f5e
HPatel000 <patelheli1711@gmail.com>
2022-02-08 16:15:12 +0530

    initial commit

---
+++ src/test/java/com/user/management/ums/UmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.management.ums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
